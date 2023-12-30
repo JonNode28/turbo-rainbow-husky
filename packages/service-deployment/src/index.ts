7b30e63bbfdcb0a6a5d8f8de1e29ab497d517499
Jon Pritchard <jon@node28.com>
2023-12-30 10:23:04 +0000

    Set PL password

--- packages/service-deployment/src/index.ts
+++ packages/service-deployment/src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander'
-import { exec, pwd, ls, set } from 'shelljs'
+import { exec, pwd, ls, set, env } from 'shelljs'
 import fs from 'fs/promises'
 
 const program = new Command()
@@ -19,6 +19,7 @@ program.command('deploy')
     if(process.env.CI !== 'true'){
         branch = exec('git rev-parse --abbrev-ref HEAD')
     }
+    env['PULUMI_CONFIG_PASSPHRASE'] = ''
     const configString = await fs.readFile(`./${options.config}`, 'utf-8')
       const config = JSON.parse(configString);
     console.log(`Deploying with config: ${JSON.stringify(config)}`)
